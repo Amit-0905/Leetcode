03882f2758d2da8a405484d86115cb6b2d1e038f
Amit <114171191+Amit-0905@users.noreply.github.com>
2023-08-31 02:12:48 +0530

    Update number-of-occurrence.cpp

--- Easy/Number of occurrence/number-of-occurrence.cpp
+++ Easy/Number of occurrence/number-of-occurrence.cpp
@@ -12,44 +12,49 @@ public:
 		of occurrences of x, otherwise returns 0. */
 	int count(int arr[], int n, int x) {
 	    // code here
-	    int f_occ=0;
-	    int l_occ=-1;
 	    
-	    int start=0;
-	    int end=n-1;
-	    // start occurence
-	    while(start<=end){
-	        int mid = start + (end-start)/2;
-	        if(arr[mid]==x){
-	            f_occ=mid;
-	            end=mid-1;
-	        }
-	        else if(arr[mid]<x){
-	            start=mid+1;
-	        }
-	        else{
-	            end=mid-1;
-	        }
-	    }
-	    start=0;
-	    end=n-1;
-	    while(start<=end){
-	        int mid = start + (end-start)/2;
-	        if(arr[mid]==x){
-	            l_occ=mid;
-	            start=mid+1;
-	        }
-	        else if(arr[mid]<x){
-	            start=mid+1;
-	        }
-	        else{
-	            end=mid-1;
-	        }
-	    }
+	   // ----------------------using lower & upper bound-----------------
+	   auto f_occ=lower_bound(arr,arr+n,x);
+	   auto l_occ = upper_bound(arr,arr+n,x);
+	    
+	    // ------------------------binary search--------------------
+	   // int f_occ=0;
+	   // int l_occ=-1;
+	   // int start=0;
+	   // int end=n-1;
+	   // // start occurence
+	   // while(start<=end){
+	   //     int mid = start + (end-start)/2;
+	   //     if(arr[mid]==x){
+	   //         f_occ=mid;
+	   //         end=mid-1;
+	   //     }
+	   //     else if(arr[mid]<x){
+	   //         start=mid+1;
+	   //     }
+	   //     else{
+	   //         end=mid-1;
+	   //     }
+	   // }
+	   // start=0;
+	   // end=n-1;
+	   // while(start<=end){
+	   //     int mid = start + (end-start)/2;
+	   //     if(arr[mid]==x){
+	   //         l_occ=mid;
+	   //         start=mid+1;
+	   //     }
+	   //     else if(arr[mid]<x){
+	   //         start=mid+1;
+	   //     }
+	   //     else{
+	   //         end=mid-1;
+	   //     }
+	   // }
 	    if(l_occ-f_occ<0){
 	        return 0;
 	    }
-	    return l_occ-f_occ+1;
+	    return l_occ-f_occ;
 	}
 };
 
