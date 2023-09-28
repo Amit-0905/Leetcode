041429f41680457ad106582f5a5b9c87f55b9176
Amit <114171191+Amit-0905@users.noreply.github.com>
2023-09-28 11:41:02 +0530

    Time: 2 ms (50.49%), Space: 6.3 MB (14.98%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,9 +1,34 @@
+// class Solution {
+// public:
+//     int mySqrt(int x) {
+//         int start=1;
+//         int end=x;
+//         int ans=0;
+//         while(start<=end){
+//             long int mid = start + (end-start)/2;
+//             if(mid*mid==x){
+//               return mid;
+//             }
+//             else if(mid*mid>x){
+//                 end=mid-1;
+//             }
+//             else{
+//                 ans=mid;
+//                 start=mid+1;
+//             }
+//         }
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     int mySqrt(int x) {
         int start=1;
-        int end=x;
+        int end=x/2;
         int ans=0;
+        if (x==1){
+          return 1;
+        }
         while(start<=end){
             long int mid = start + (end-start)/2;
             if(mid*mid==x){
