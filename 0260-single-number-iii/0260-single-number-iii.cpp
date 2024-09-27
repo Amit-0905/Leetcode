d30714209a12fd3cd7a9f4f95001822d2750405b
Amit <114171191+Amit-0905@users.noreply.github.com>
2024-09-28 00:09:13 +0530

    Time: 5 ms (78.75%), Space: 12.5 MB (88.93%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        int num1=0;
+        int num2=0;
+        long long int xor_sum=0;
+        for(int i=0;i<nums.size();i++){
+            xor_sum=xor_sum^nums[i];
+        }
+        int rightmost_bit = xor_sum^(xor_sum & (xor_sum-1));
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]&rightmost_bit){
+                num1=num1^nums[i];
+            }
+            else{
+                num2=num2^nums[i];
+            }
+        }
+        vector<int>ans={num1,num2};
+        return ans;
+    }
+};
