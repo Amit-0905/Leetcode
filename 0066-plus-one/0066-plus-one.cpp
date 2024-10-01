dc15d0cbf8d84b002f9adb151b66367d93f228eb
Amit <114171191+Amit-0905@users.noreply.github.com>
2024-10-01 21:19:33 +0530

    Time: 5 ms (27.04%), Space: 10.5 MB (49%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,25 +1,3 @@
-// class Solution {
-// public:
-//     vector<int> plusOne(vector<int>& digits) {
-//         int i= digits.size()-1;
-//         while(true){
-//             if(digits[i]==9){
-//                 if(i-1){
-//                     i=i-1;
-//                 }
-//                 digits.push_back(digits[i]);
-//                 digits[i]=1;
-//                 break;
-//             }
-//             else{
-//                 digits[i]++;
-//                 break;
-//             }
-//         }
-//         return digits;
-//     }
-// };
-
 class Solution {  
 public:  
     vector<int> plusOne(vector<int>& digits) {  
