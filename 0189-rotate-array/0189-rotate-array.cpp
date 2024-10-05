2d454a2390e9b1a1baca0ba52f1c089b6dad9f19
Amit <114171191+Amit-0905@users.noreply.github.com>
2024-10-05 05:52:27 +0530

    Time: 22 ms (43.71%), Space: 27.5 MB (92.51%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        int n=nums.size();
-        k %= n;
-        if(nums.size()<=k || k==0){
-            return;
-        }
-        vector <int>move;
-        for(int i=n-k;i<n;i++){
-            move.push_back(nums[i]);
-        }
-        for(int i=n-1;i>=k;i--){
-            swap(nums[i],nums[i-k]);
-        }
-        for(int i=0;i<k;i++){
-            nums[i]=move[i];
-        }
+        // int n=nums.size();
+        // k %= n;
+        // if(nums.size()<=k || k==0){
+        //     return;
+        // }
+        // vector <int>move;
+        // for(int i=n-k;i<n;i++){
+        //     move.push_back(nums[i]);
+        // }
+        // for(int i=n-1;i>=k;i--){
+        //     swap(nums[i],nums[i-k]);
+        // }
+        // for(int i=0;i<k;i++){
+        //     nums[i]=move[i];
+        // }
+
+        int n = nums.size();  
+        if (n == 0) return;
+        k %= n;  
+        if (k == 0) return;  
+        reverse(nums.begin(), nums.end()); 
+        reverse(nums.begin(), nums.begin() + k); 
+        reverse(nums.begin() + k, nums.end());
     }
 };
